c6a474c1fc027da841ce5f53da4cb40823ad5de5
IMRAN <IMRAN@SE12>
2016-10-26 11:48:13 +0800

    Add project files.

---
+++ WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    public class Class1
+    {
+        public Interface1 Interface1
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public Class2 Class2
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+    }
+}
